94a2f37a8eaa9f668ba00ba404c7682e5e59c49f
marilyn-814 <ucapmux@ucl.ac.uk>
2022-10-20 11:00:44 +0100

    Some initialisation from MDJ

---
+++ group.py
@@ -0,0 +1,23 @@
+"""An example of how to represent a group of acquaintances in Python."""
+
+# Your code to go here...
+
+my_group = {
+}
+
+# person_1 = {
+#     'name':'Jill',
+#     'age':26,
+#     'job':'biologist',
+#     'connection':"Zalika's friend and John's partner"
+# }
+
+people_dict = {"Jill","Zalika","John","Nash"}
+
+person_1 = [26,"biologist","Zalika's friend and John's partner"]
+
+person_2 = [28,"artist","Jill's friend"]
+
+person_3 = [27,"writer","Jill's partner"]
+
+person_4 = [34,"chef","John's cousin and Zalika's landlord"]
